e0dfa971e050e85c35cd9148500ab4f556aa26a6
Sharon Yelverton <ladysharonyel@me.com>
2020-02-05 08:30:29 -0500

    added random quotes/error message alert/modal

--- public/assets/js/tabs.js
+++ public/assets/js/tabs.js
@@ -105,4 +105,11 @@ function submitPost(post) {
   $.post("/api/exercises", post
     
   )};
+// Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach('.carousel', options);
 
+// To access to bulmaCarousel instance of an element
+const element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+}
